873feba6c9e63d0c0ec4caf49f68d37241a3854b
Chris Devalme <cdevalme@gmail.com>
2026-05-06 20:27:31 -0400

    - File structure created - Routing & ReactDOM updates

---
+++ src/client.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+const URL = 'atvrljgjnmhpetbygyqz';
+const API_KEY = 'sb_publishable_s5OrskR4SfeH0LwF2Hu4jw_xli4B9wJ';
+export const supabase = createClient(URL, API_KEY);
+
