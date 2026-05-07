424c80a52ed432bdbd4611bdb68cafe2b56d5e9a
Chris Devalme <cdevalme@gmail.com>
2026-05-07 13:55:27 -0400

    Supabase connection correction

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const URL = 'atvrljgjnmhpetbygyqz';
+const URL = 'https://atvrljgjnmhpetbygyqz.supabase.co';
 const API_KEY = 'sb_publishable_s5OrskR4SfeH0LwF2Hu4jw_xli4B9wJ';
 export const supabase = createClient(URL, API_KEY);
 
